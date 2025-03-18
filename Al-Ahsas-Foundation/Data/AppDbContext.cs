b7fa58b5e954d5a0aa6caafb7aee93eecc924145
Habib Khalqi <103850736+Habibkhalqi@users.noreply.github.com>
2025-03-18 14:07:38 +0500

    VSproject
    upload VS project of al ahsas

---
+++ Al-Ahsas-Foundation/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Al_Ahsas_Foundation.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ Al-Ahsas-Foundation/Models/Case.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Al_Ahsas_Foundation.Models
+{
+    public class Case
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the title of the case")]
+        public string  Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter the description of the case")]
+        public string  Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter the amount of the case")]
+        public string Amount { get; set; }
+
+        [Required(ErrorMessage = "Please enter the category of the case")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Please enter the details of the case")]
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "Please enter the image of the case")]
+        public string  Vedio { get; set; }
+
+        [Required(ErrorMessage = "Please enter the image of the case")]
+        public Cause Cause { get; set; }
+
+    }
+}
